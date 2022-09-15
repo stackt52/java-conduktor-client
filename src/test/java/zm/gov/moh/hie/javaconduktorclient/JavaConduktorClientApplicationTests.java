5bdff1b7cba86f1173785e32475f9f95635f1cc1
Terence Kaite <terrencekaite@gmail.com>
2022-09-15 05:52:14 +0200

    Create JavaConduktorClientApplicationTests.java

---
+++ src/test/java/zm/gov/moh/hie/javaconduktorclient/JavaConduktorClientApplicationTests.java
@@ -0,0 +1,13 @@
+package zm.gov.moh.hie.javaconduktorclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConduktorClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
